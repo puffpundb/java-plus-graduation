53863f568e6c08746205504ae594f5f8b3891709
Danil Shirykalov <shirds@yandex.ru>
2026-05-16 20:29:30 +0500

    Исправлены n+1 запросы 2

--- stats/analyzer/src/main/java/ru/practicum/analyzer/repository/SimilarityRepository.java
+++ stats/analyzer/src/main/java/ru/practicum/analyzer/repository/SimilarityRepository.java
@@ -1,6 +1,8 @@
 package ru.practicum.analyzer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.analyzer.entity.Similarity;
 
 import java.util.Collection;
@@ -10,5 +12,8 @@ import java.util.Optional;
 public interface SimilarityRepository extends JpaRepository<Similarity, Long> {
 	Optional<Similarity> findByEvent1AndEvent2(Long e1, Long e2);
 	List<Similarity> findByEvent1OrEvent2(Long event1, Long event2);
-	List<Similarity> findAllByEventAInOrEventBIn(Collection<Long> eventIdsA, Collection<Long> eventIdsB);
+
+	@Query("SELECT s FROM Similarity s WHERE s.event1 IN :eventIdsA OR s.event2 IN :eventIdsB")
+	List<Similarity> findAllByEventAInOrEventBIn(@Param("eventIdsA") Collection<Long> eventIdsA,
+												 @Param("eventIdsB") Collection<Long> eventIdsB);
 }
